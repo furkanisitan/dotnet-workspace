42210acdd3fc59dd1592619e5ea2e9d7144d56ac
furkanisitan <furkanisitan96@gmail.com>
2022-07-10 20:57:59 +0300

    Add ApplicationDbContext class

---
+++ src/EFCore/DotNetWorkspace.EFCore.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DotNetWorkspace.EFCore.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetWorkspace.EFCore.DataAccess;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Customer> Customers => Set<Customer>();
+    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<Payment> Payments => Set<Payment>();
+    public DbSet<Product> Products => Set<Product>();
+
+}
