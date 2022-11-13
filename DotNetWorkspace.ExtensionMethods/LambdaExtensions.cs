545c506aa3ea8897d399c9e40abb033596a47e18
furkanisitan <furkanisitan96@gmail.com>
2022-11-14 00:21:51 +0300

    Create ExtensionMethods project
    - Create LambdaExtensions and TypeExtensions classes
    - Add LambdaExtensions#GetPropertyInfo method
    - Add LambdaExtensions#GetPropertyName method
    - Add TypeExtensions#IsAssignableToGenericType method

---
+++ DotNetWorkspace.ExtensionMethods/LambdaExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+namespace DotNetWorkspace.ExtensionMethods
+{
+    /// <summary>
+    /// This class contains extension methods for lambda expressions.
+    /// </summary>
+    public static class LambdaExtensions
+    {
+        public static PropertyInfo GetPropertyInfo<TType, TReturn>(this Expression<Func<TType, TReturn>> property)
+        {
+            LambdaExpression lambda = property;
+            var memberExpression = lambda.Body is UnaryExpression expression
+                ? (MemberExpression)expression.Operand
+                : (MemberExpression)lambda.Body;
+
+            return (PropertyInfo)memberExpression.Member;
+        }
+
+        public static string GetPropertyName<TType, TReturn>(this Expression<Func<TType, TReturn>> property) =>
+            property.GetPropertyInfo().Name;
+    }
+}
---
+++ DotNetWorkspace.ExtensionMethods/TypeExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace DotNetWorkspace.ExtensionMethods
+{
+    /// <summary>
+    /// This class contains extension methods for the <see cref="Type"/>.
+    /// </summary>
+    public static class TypeExtensions
+    {
+        /// <summary>
+        /// Determines whether the current type can be assigned to a variable of the specified <paramref name="genericType"/>.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="genericType">The generic type to compare with the current type.</param>
+        /// <returns><see langword="true"/> if the current type can be assigned to the <paramref name="genericType"/>, otherwise <see langword="false"/>.</returns>
+        public static bool IsAssignableToGenericType(this Type type, Type? genericType)
+        {
+            if (genericType is null)
+                return false;
+
+            return type.IsAssignableTo(genericType) ||
+                   type.IsGenericType && type.GetGenericTypeDefinition() == genericType ||
+                   type.GetInterfaces().Where(x => x.IsGenericType).Any(x => x.GetGenericTypeDefinition() == genericType) ||
+                   type.BaseType is not null && type.BaseType.IsAssignableToGenericType(genericType);
+        }
+
+    }
+}
